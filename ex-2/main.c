9fa4770eded9836f25e11806541303cdda19a7b4
alexisdamien <alexisdamien@icloud.com>
2025-10-07 09:36:00 +0000

    exercices 1 2 et 3

---
+++ ex-2/main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	int number = 1342;
+	printf("%d\n", number);
+	exit(0);
+}
---
+++ ex-3/main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	char * name = "Alexis";
+	printf("Bonjour je suis %s\n", name);
+	exit(0);
+}
+
